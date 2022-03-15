4daa6fe263a0beb900211e5d828a395b2a47cd19
bisrat-demere <bisratdemere@gmail.com>
2022-03-15 19:23:12 +0300

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - putchar file
+ * description - a program that prints _putchar
+ * Return: always 0
+**/
+
+int main(void)
+{
+	char a[] = "_putchar";
+	int b = 0;
+
+	while (b <= 8)
+	{
+		putchar (a[b]);
+		b++;
+	}
+	putchar ('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+alx low level programming
+0x02 function nested loops
