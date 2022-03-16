126449ad96f22706a1195ab6923f963a3907fe61
bisrat-demere <bisratdemere@gmail.com>
2022-03-16 23:20:45 +0300

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,21 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		for (; n <= 98; n++)
-			printf("%i, ", n);
+		{
+			printf("%i", n);
+			if (n != 98)
+				printf(", ");
+		}
 		printf("\n");
 	}
 	else
 	{
 		for (; n >= 98; n--)
-			printf("%i, ", n);
+		{
+			printf("%i", n);
+			if (n != 98)
+				printf(", ");
+		}
 		printf("\n");
 	}
 }
