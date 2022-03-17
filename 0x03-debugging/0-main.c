967ed3700c3031dfc1f16a984b5553d41cfb0b76
bisrat-demere <bisratdemere@gmail.com>
2022-03-18 00:21:25 +0300

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
-#include "main.h"
+#include"main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - positive or negative
 * Return: 0
-**/
+*/
 
 int main(void)
 {
 	int i;
-	
+
 	i = 98 % 2;
 	positive_or_negative(i);
 
