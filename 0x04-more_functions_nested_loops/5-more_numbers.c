f927648decab162da28da53412c144bff5f200b3
bisrat-demere <bisratdemere@gmail.com>
2022-03-17 21:39:04 +0300

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,37 @@ void more_numbers(void)
 
 	while (n < 10)
 	{
-		int alph = 0;
+		int alph = 0, a = 0, b = 1, c = 2, d = 3, e = 4;
 
-		while (alph <= 9)
+		while (alph <= 14)
 		{
-			_putchar(alph + '0');
+			if (alph < 10)
+				_putchar(alph + '0');
+			else if (alph < 11)
+			{
+				_putchar(b + '0');
+				_putchar(a + '0');
+			}
+			else if (alph < 12)
+			{
+			_putchar(b + '0');
+			_putchar(b + '0');
+			}
+			else if (alph < 13)
+			{
+				_putchar(b + '0');
+				_putchar(c + '0');
+			}
+			else if (alph < 14)
+			{
+				_putchar(b + '0');
+				_putchar(d + '0');
+			}
+			else if (alph < 15)
+			{
+				_putchar(b + '0');
+				_putchar(e + '0');
+			}
 			alph++;
 		}
 		_putchar('\n');
