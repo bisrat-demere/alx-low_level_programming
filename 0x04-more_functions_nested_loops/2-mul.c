63470a268e5bff5909bb7eafe67125aeeaf0bcbf
bisrat-demere <bisratdemere@gmail.com>
2022-03-17 08:49:21 +0300

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplication
+ * @a: input one
+ * @b: input two
+ * description - return 1 if c is u.c
+ * Return: mult
+**/
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = (a * b);
+	return (mult);
+}
