4004d57707fe2fc0c55acf459fddeda61d982554
bisrat-demere <bisratdemere@gmail.com>
2022-03-24 00:19:23 +0300

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,14 +13,14 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[n] != '\0' && s2[n] != '\0')
 	{
-		if (strcmp(s1, s2) == 0)
+		if (s1[n] != s2[n])
 		{
 			a = (s1[n] - s2[n]);
+			break;
 		}
 		else
 		{
 			a = (s1[n] - s2[n]);
-			break;
 		}
 		n++;
 	}
