e12ebc52b19730388f1292c78927a7989de1adc4
bisrat-demere <bisratdemere@gmail.com>
2022-03-23 19:52:58 +0300

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - concatenates
+ * @dest: char 1
+ * @src: char 2
+ * Return: 0
+**/
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
