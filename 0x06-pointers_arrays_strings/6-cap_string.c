ca096c2ec162d504d65268d53fb335d734d6dfdb
bisrat-demere <bisratdemere@gmail.com>
2022-03-24 01:09:15 +0300

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *cap_string - upper to lower
+ * @g: char
+ * Return: g
+**/
+
+char *cap_string(char *g)
+{
+	int n;
+
+	for (n = 0; g[n] != '\0'; n++)
+	{
+		if (g[0] > 96 && g[0] < 123)
+			g[0] = (g[0] - 32);
+		if (g[n] == 32)
+		{
+			if (g[n + 1] > 96 && g[n + 1] < 123)
+				g[n + 1] = (g[n + 1] - 32);
+		}
+	}
+	return (g);
+}
