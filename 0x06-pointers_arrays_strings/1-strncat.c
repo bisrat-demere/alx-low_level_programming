5b77cd0a6942de003e243c58edaa5d555b61a67b
bisrat-demere <bisratdemere@gmail.com>
2022-03-23 20:47:33 +0300

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncat - concatenates
+ * @dest: char one
+ * @src: char two
+ * @n: number of char to be concat
+ * Return: dest**/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
