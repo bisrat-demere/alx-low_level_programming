e86c431d42f1ff527acdb5cc118ea12c6733a92a
Bisrew <bisratdemere@gmail.com>
2022-03-24 21:50:03 -0700

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,17 +7,21 @@
 
 char *leet(char *g)
 {
-	int n;
+	int n, i = 0;
 	int low[] = {97, 101, 111, 116, 108};
 	int up[] = {65, 69, 79, 84, 76};
 	int num[] = {52, 51, 48, 55, 49};
 
 	for (n = 0; g[n] != '\0'; n++)
 	{
-		if (g[n] == low[n] || g[n] == up[n])
+		while (i < 5)
 		{
-			g[n] = num[n];
-			break;
+			if (g[n] == low[i] || g[n] == up[i])
+			{
+				g[n] = num[i];
+				break;
+			}
+			i++
 		}
 	}
 	return (g);
