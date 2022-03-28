674b3bf0c1da8829ed6321164fac7b4cc3837a7f
bisrat-demere <bisratdemere@gmail.com>
2022-03-29 00:56:44 +0300

    0x07

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - a fun
+ * @s: pntr
+ * @c: char
+ * Return: 0
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return ('\0');
+}
