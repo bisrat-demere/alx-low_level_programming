4fd1d943331c1be6215bc87053a86caa75e17487
bisrat-demere <bisratdemere@gmail.com>
2022-04-04 17:13:27 +0300

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function
+ *@argc: int
+ *@argv: char
+ *Return: 0 and 1
+**/
+
+int main(int argc, char *argv[])
+{
+	int i, j = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			j = j * atoi(argv[i]);
+		}
+		printf("%d\n", j);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
