4089ee16017a9bf2b775d2b7a0c97c16558b4533
bisrat-demere <bisratdemere@gmail.com>
2022-04-05 12:09:31 +0300

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - function that add command line argument
  * @argc: int
@@ -11,20 +12,23 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc > 1)
+	if (argc == 1)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if (argv[i] >= '48' && argv[i] <= '57')
-			{
-				sum += atoi(argv[i]);
-			}
-		}
-		printf("%d\n", sum);
+		printf("0\n");
 	}
 	else
 	{
-		printf("0\n");
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(*argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
 	}
 	return (0);
 }
