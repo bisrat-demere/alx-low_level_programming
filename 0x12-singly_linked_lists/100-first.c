5a3b39723118605afe97d6120a9b3a9fc0bfe425
bisrat-demere <bisratdemere@gmail.com>
2022-04-23 00:57:32 +0300

    adva

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-
+#include "lists.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * fun_before - function execute before main
  */
-int main(void)
+void fun_before(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("%s", "You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
