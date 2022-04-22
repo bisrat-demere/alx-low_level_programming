461ee8c484c0330ece1f6408c5817ec01038c087
bisrat-demere <bisratdemere@gmail.com>
2022-04-23 00:55:06 +0300

    adva

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,25 @@
+extern printf
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,format
+   mov rsi,message
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   message: db "Hello, Holberton", 0
+  	format: db "%s", 10, 0
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+
